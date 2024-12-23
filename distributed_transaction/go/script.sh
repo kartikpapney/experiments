7cebae18e17d4ca1ed4f08a56e804a92e696765a
Kartik Papney <pdt.kartik@gmail.com>
2024-12-24 00:51:02 +0530

    update: benchmark

--- distributed_transaction/go/script.sh
+++ distributed_transaction/go/script.sh
@@ -2,5 +2,5 @@ echo "\nWithout threads"
 go run main.go normal
 echo "\nWith threads and normal lock"
 go run main.go lock
-echo "\nWithout threads and skip lock"
+echo "\nWith threads and skip lock"
 go run main.go skip-lock
