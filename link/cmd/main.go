3787714b6adac78ff638904608e30d2b516bbb71
Kartik Papney <kartik.papney@wobot.ai>
2025-03-20 01:40:04 +0530

    feat: webpage link parser

---
+++ link/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/experiments/link"
+)
+
+func main() {
+	filename := flag.String("file", "ex1.html", "the HTML file to parse for links")
+	flag.Parse()
+
+	s, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	links, err := link.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, l := range links {
+		fmt.Printf("%+v, %+v\n", l.Href, l.Text)
+	}
+}
---
+++ link/link.go
@@ -0,0 +1,51 @@
+package link
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string `json:"href"`
+	Text string `json:"text"`
+}
+
+func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	return getAllLinks(doc), nil
+}
+
+func getAllLinks(n *html.Node) []Link {
+	var links []Link
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				txt := extractText(n)
+				links = append(links, Link{a.Val, txt})
+			}
+		}
+	}
+
+	// loop through all children nodes and recursively call getAllLinks
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		exLinks := getAllLinks(c)
+		links = append(links, exLinks...)
+	}
+	return links
+}
+
+func extractText(n *html.Node) string {
+	var text string
+	if n.Type != html.ElementNode && n.Data != "a" && n.Type != html.CommentNode {
+		text = n.Data
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		text += extractText(c)
+	}
+	return strings.Trim(text, "\n")
+}
